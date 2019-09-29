a6f8521f3d6051138f633f524caa82c489a1bf30
Asep Kurniawan <askinas@Aseps-MacBook-Pro.local>
2019-09-30 00:39:50 +0700

    initialize

---
+++ app/Http/Controllers/CatchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatchController extends Controller
+{
+    /**
+     * Processing Order
+     * 
+     * @return void 
+     */
+    public function processOrder(){
+        echo "ini test\n";
+    }
+    
+}
